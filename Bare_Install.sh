a76f7084d6182903ea721fb992f96f34e610bcd7
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2022-09-10 12:33:04 +0000

    usb os

--- Bare_Install.sh
+++ Bare_Install.sh
@@ -6,36 +6,23 @@ if [[ ! $(curl -Is http://www.google.com/ | head -n 1) =~ "200 OK" ]]; then
 	read
 fi
 
-# make 2 partitions on the disk.
-parted -s /dev/sda mktable gpt
-parted -s /dev/sda mkpart "'EFI File System'" fat32 0% 300m
-parted -s /dev/sda set 1 esp on
-parted -s /dev/sda mkpart "'Linux'" btrfs 300m 100%
-
 # make filesystems
 # /boot
-mkfs.fat -F32 /dev/sda1
+mkfs.fat -F32 /dev/sdc1
 # /
-mkfs.btrfs /dev/sda2
+mkfs.ext4 -O "^has_journal" /dev/sdc2
 
 # set up /mnt
-mount /dev/sda2 /mnt
-mkdir /mnt/boot
-cd /mnt
-btrfs su cr @
-btrfs su cr @home
-cd
-umount /mnt
-mount -o noatime,compress=zstd,space_cache=v2,subvol=@ /dev/sda2 /mnt
-mkdir -p /mnt/{boot,home}
-mount -o noatime,compress=zstd,space_cache=v2,subvol=@home /dev/sda2 /mnt/home
-mount /dev/sda1 /mnt/boot
+mount /dev/sdc2 /mnt
+mkdir -p /mnt/boot/efi
+mount /dev/sdc1 /mnt/boot/efi
 
 grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/;s/^#Color$/Color/" /etc/pacman.conf
 
 # install base packages (take a coffee break if you have slow internet)
-pacstrap /mnt base base-devel linux linux-firmware linux-headers reflector sudo git vim btrfs-progs grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog xdg-user-dirs --noconfirm --needed
+pacstrap /mnt base linux linux-firmware vim grub efibootmgr networkmanager network-manager-applet mtools dosfstools reflector git base-devel linux-headers bluez bluez-utils cups xdg-utils xdg-user-dirs --noconfirm --needed
+
 genfstab -U /mnt >>/mnt/etc/fstab
 
 cp -r /root/Q-OS /mnt/root/
--- Chroot_Install.sh
+++ Chroot_Install.sh
@@ -16,6 +16,9 @@ addrootuserpass() {
     unset pas1 pas2
 }
 
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+
 # Update reflector list
 iso=$(curl -4 ifconfig.co/country-iso)
 reflector -a 47 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
@@ -25,10 +28,10 @@ pacman -Syy
 sed -i '/^#en_US.UTF-8* /s/^#//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >>/etc/locale.conf
-echo "QWINOS" >>/etc/hostname
+echo "archlinux" >>/etc/hostname
 echo "127.0.0.1 localhost" >>/etc/hosts
 echo "::1       localhost" >>/etc/hosts
-echo "127.0.1.1 qwinos.localdomain qwinos" >>/etc/hosts
+echo "127.0.1.1 archlinux.localdomain archlinux" >>/etc/hosts
 
 # Add root user's password
 getrootpass || error "Root user error"
@@ -36,46 +39,12 @@ addrootuserpass || error "Root user password error"
 clear
 
 # Determine processor type and install microcode
-proc_type=$(lscpu | awk '/Vendor ID:/ {print $3}')
-case "$proc_type" in
-GenuineIntel)
-    print "Installing Intel microcode"
-    pacman -S --noconfirm intel-ucode
-    proc_ucode=intel-ucode.img
-    ;;
-AuthenticAMD)
-    print "Installing AMD microcode"
-    pacman -S --noconfirm amd-ucode
-    proc_ucode=amd-ucode.img
-    ;;
-esac
+print "Installing Intel microcode"
+pacman -S --noconfirm intel-ucode
 
-# Graphics Drivers find and install
-if lspci | grep -E "NVIDIA|GeForce"; then
-    pacman -S nvidia --noconfirm --needed
-    nvidia-xconfig
-elif lspci | grep -E "Radeon"; then
-    pacman -S xf86-video-amdgpu --noconfirm --needed
-elif lspci | grep -E "Integrated Graphics Controller"; then
-    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
-elif lspci | grep -E "VMware"; then
-    pacman -S lib32-vulkan-radeon lib32-vulkan-radeon-mesa --needed --noconfirm
-    pacman -S xf86-video-vmware --noconfirm --needed
-elif lspci | grep -E "VGA compatible controller"; then
-    pacman -S xf86-video-vesa --noconfirm --needed
-fi
-
-# Grub Install
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=QWinGRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Window manager & Other install
-curl https://raw.githubusercontent.com/QWinOS/Q-Script/master/Q-Script.sh --output /tmp/Q-Script.sh
-chmod +x /tmp/Q-Script.sh
-/tmp/./Q-Script.sh
-
-# Create the directories Desktop, Documents, Downloads, Music, Pictures, Public, Templates, Videos
-xdg-user-dirs-update
+print "Installing AMD microcode"
+pacman -S --noconfirm amd-ucode
 
 # Enable Network Manager
 systemctl enable NetworkManager
+systemctl enable bluetooth
